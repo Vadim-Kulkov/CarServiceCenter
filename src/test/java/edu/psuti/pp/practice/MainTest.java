51f42796b63e5e5ffd3ca2c3e7acf3386045ebd0
Кульков Вадим Дмитриевич <200131@edu.psuti.ru>
2021-09-13 21:23:25 +0400

    274122533ad8a2546bc0d2d32e2693a204b1d1b1

---
+++ src/test/java/edu/psuti/pp/practice/MainTest.java
@@ -0,0 +1,12 @@
+package edu.psuti.pp.practice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        assertFalse(false);
+    }
+}
